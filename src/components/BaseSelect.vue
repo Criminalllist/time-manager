ffa008f06350d8f8031e49138973ec892c36e22a
Criminalllist <vrazlada@mail.ru>
2024-04-19 20:27:41 +0300

    feat: add base elements: basebutton/ baseselect

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full flex gap-2">
+    <BaseButton>
+      <XMarkIcon class="h-8" />
+    </BaseButton>
+
+    <select v-bind="$attrs" class="w-full truncate rounder bg-gray-100 py-1 px-2">
+      <option value="rest" selected disabled>{{ placeholder }}</option>
+      <option
+        v-for="{ value, label } in activities"
+        :key="value"
+        :value="value"
+        :selected="value === selected"
+      >
+        {{ label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+import BaseButton from './BaseButton.vue'
+import { XMarkIcon } from '@heroicons/vue/24/outline'
+defineProps(['activities', 'placeholder', 'selected'])
+
+defineOptions({
+  inheritAttrs: false
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -2,25 +2,17 @@
   <li class="relative flex flex-column gap-2 border-t border-gray-200 py-10 px-4">
     <a href="#" :class="hourLinksClasses">{{ timelineItem.hour }}:00</a>
 
-    <div class="w-full flex gap-2">
-      <button
-        class="rounded bg-gray-100 p-3 enables:hover:bg-gray-200 disabled:cursor-not-allowed disabled:opacity-50"
-      >
-        <XMarkIcon class="h-8" />
-      </button>
-
-      <select name="activities" class="w-full truncate rounder bg-gray-100 py-1 px-2">
-        <option value="rest" selected disabled>Rest</option>
-        <option v-for="{ value, label } in activities" :key="value" :value="value">
-          {{ label }}
-        </option>
-      </select>
-    </div>
+    <BaseSelect
+      :activities="activities"
+      placeholder="Rest"
+      :selected="setSelectedOption"
+      name="activities"
+    />
   </li>
 </template>
 
 <script setup>
-import { XMarkIcon } from '@heroicons/vue/24/outline'
+import BaseSelect from '@/components/BaseSelect.vue'
 const props = defineProps(['timelineItem'])
 
 const activities = [
@@ -29,16 +21,18 @@ const activities = [
   { value: 3, label: 'Training' }
 ]
 
-const currentHour = () => {
-  return props.timelineItem.hour === new Date().getHours()
-    ? 'bg-purple-900 font-black text-white'
-    : 'bg-gray-100 text-gray-500'
-}
+const setSelectedOption = 0
 
 const hourLinksClasses = [
   'absolute -top-4 left-1/2 -translate-x-1/2 rounder  px-2 font-mono text-lg',
   currentHour()
 ]
+
+function currentHour() {
+  return props.timelineItem.hour === new Date().getHours()
+    ? 'bg-purple-900 font-black text-white'
+    : 'bg-gray-100 text-gray-500'
+}
 </script>
 
 <style lang="scss" scoped></style>
